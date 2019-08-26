c6f9e7f7a3523a36815d0e0291038b8728a917e8
unknown <https://github.com/sujithreddy9493/suij.git>
2019-08-26 08:23:45 +0530

    Form validation

--- angular/basics/src/app/app.module.ts
+++ angular/basics/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms'
-
 import { AppComponent } from './app.component';
 import { TempalteComponent } from './tempalte/tempalte.component'
 
@@ -9,6 +8,7 @@ import { TempalteComponent } from './tempalte/tempalte.component'
   declarations: [
     AppComponent,
     TempalteComponent,
+    
   ],
   imports: [
     BrowserModule,
--- angular/basics/src/app/tempalte/tempalte.component.ts
+++ angular/basics/src/app/tempalte/tempalte.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators} from '@angular/forms'
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 @Component({
   selector: 'app-tempalte',
   templateUrl: './tempalte.component.html',
   styleUrls: ['./tempalte.component.css']
 })
 export class TempalteComponent implements OnInit {
+  
   title = 'Sujithreddy';
   S = 'by @apple';
 
@@ -16,5 +18,6 @@ export class TempalteComponent implements OnInit {
   formDetails(d){
    console.log(d);
   }
+  
 
 }
